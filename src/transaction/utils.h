d8df7b4c3f4be9340c2869445e3f0fe6e0cce238
xiemylogos <xiemylogos@gmail.com>
2025-05-28 17:31:37 +0800

    optimize code comments

--- src/transaction/utils.h
+++ src/transaction/utils.h
@@ -58,5 +58,14 @@ static inline bool methodcmp(const tx_parameter_t *param, const char *method_nam
     return param->len == strlen(method_name) && memcmp(param->data, method_name, param->len) == 0;
 }
 
-// Validate BIP-32 path prefix (44'/1024' or 44'/888')
+/**
+ * Validate a BIP-32 path prefix (e.g., 44'/1024' or 44'/888').
+ *
+ * @param[in] path
+ *   Pointer to the array representing the BIP-32 path.
+ * @param[in] path_len
+ *   Length of the path array.
+ *
+ * @return true if the path has a valid BIP-44 prefix, false otherwise.
+ */
 bool is_valid_bip44_prefix(uint32_t *path, uint8_t path_len);
